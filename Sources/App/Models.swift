87f4690843020313a0b667421311d450b6d94e56
Anis Abdat <158279771+AnisAb2002@users.noreply.github.com>
2026-04-03 09:05:39 +0000

    Création du model Exercice

--- Sources/App/Models.swift
+++ Sources/App/Models.swift
@@ -13,3 +13,25 @@ struct Utilisateur: Codable, Sendable {
     var IMC: Double
     var scoreUtilisateur: Int
 }
+
+// MARK: - Exercice
+struct Exercice: Codable, Sendable {
+    var idExercice: Int
+    var nom: String
+    var duree: Int
+    var muscles: [String]
+    var objectif: String
+    var score: Int
+    var image: String
+}
+
+// MARK: - Séance
+struct Seance: Codable, Sendable {
+    var idSeance: Int
+    var loginUtilisateur: String
+    var duree: Int
+    var objectif: String
+    var score: Int
+    var date: Date
+    var exercices: [Exercice]
+}
