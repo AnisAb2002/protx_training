050b8a00b80e20f7d86512fae833c4b0f69ebc0d
Anis Abdat <158279771+AnisAb2002@users.noreply.github.com>
2026-04-03 09:03:14 +0000

    Création du model Utilisateur

--- Sources/App/Models.swift
+++ Sources/App/Models.swift
@@ -1,7 +1,15 @@
 import Foundation
 
-struct TaskItem: Codable, Sendable {
-    let id: Int64?
-    var title: String
-    var isCompleted: Bool
+// MARK: - Utilisateur
+struct Utilisateur: Codable, Sendable {
+    var login: String
+    var nom: String
+    var prenom: String
+    var dateNaissance: Date
+    var motDePasse: String
+    var objectif: String
+    var poids: Double
+    var taille: Double
+    var IMC: Double
+    var scoreUtilisateur: Int
 }
